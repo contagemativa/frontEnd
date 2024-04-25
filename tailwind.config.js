167b0e1c2c4d47068dd1df76e797eb05721636c1
Victor Magalhães de Souza <victor.souza@logisticmobile.com.br>
2024-04-25 14:23:03 -0300

    bTemplates - Ajuste no template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,23 @@
 module.exports = {
   content: [
     "./App/View/**/*.{html,js}",
-  ],
+    "./App/View/**/**/*.{html,js}"
+  ], 
   theme: {
     extend: {},
   },
   plugins: [
-    require("daisyui")
-  ],
+    require("daisyui")],
+  daisyui: {
+    themes: [{
+      ligth: {
+        "primary": "#B8398C",
+        "secondary": "#CC74AE",
+        "accent": "#37cdbe",
+        "neutral": "#3d4451",
+        "base-100": "#ffffff"
+      }
+    }]
+  },
 }
 
