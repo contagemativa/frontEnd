e508834264de34c51e086933d0d6b02e59a2c11b
VictorVM7 <vicmagalhaes20@gmail.com>
2024-06-19 20:53:38 -0300

    Criando classes para nova branch

---
+++ App/Models/Class/enderecoClass.js
@@ -0,0 +1,33 @@
+// enderecoClass.js
+
+class Endereco {
+    constructor({
+      id = 0,
+      cep = "",
+      rua = "",
+      numero = 0,
+      complemento = "",
+      bairro = "",
+      cidade = "",
+      estado = "",
+      pais = ""
+    }) {
+      this.id = id;
+      this.cep = cep;
+      this.rua = rua;
+      this.numero = numero;
+      this.complemento = complemento;
+      this.bairro = bairro;
+      this.cidade = cidade;
+      this.estado = estado;
+      this.pais = pais;
+    }
+  
+    // Método para exibir informações do endereço
+    getInfo() {
+      return `${this.rua}, ${this.numero}, ${this.complemento}, ${this.bairro}, ${this.cidade} - ${this.estado}, ${this.pais}, CEP: ${this.cep}`;
+    }
+  }
+  
+  module.exports = Endereco;
+  
---
+++ App/Models/Class/pessoaClass.js
@@ -0,0 +1,55 @@
+class Pessoa {
+    constructor({
+      id = 0,
+      nome = "",
+      cpf = "",
+      naturalidade = "",
+      dataDeNascimento = new Date(),
+      sexo = "",
+      corRaca = "",
+      endereco = 0,
+      usuario = "",
+      senha = ""
+    }) {
+      this.id = id;
+      this.nome = nome;
+      this.cpf = cpf;
+      this.naturalidade = naturalidade;
+      this.dataDeNascimento = new Date(dataDeNascimento);
+      this.sexo = sexo;
+      this.corRaca = corRaca;
+      this.endereco = endereco;
+      this.usuario = usuario;
+      this.senha = senha;
+    }
+  
+    // Método para exibir informações da pessoa
+    getInfo() {
+      return `Nome: ${this.nome}, CPF: ${this.cpf}, Naturalidade: ${this.naturalidade}, Data de Nascimento: ${this.dataDeNascimento.toLocaleDateString()}, Sexo: ${this.sexo}, Cor/Raça: ${this.corRaca}`;
+    }
+  
+    // Método para verificar se a senha é válida
+    verificarSenha(senha) {
+      return this.senha === senha;
+    }
+  
+    // Método para atualizar o endereço
+    atualizarEndereco(novoEndereco) {
+      this.endereco = novoEndereco;
+    }
+  
+    // Método para atualizar os dados pessoais
+    atualizarDados({ nome, cpf, naturalidade, dataDeNascimento, sexo, corRaca, usuario, senha }) {
+      if (nome) this.nome = nome;
+      if (cpf) this.cpf = cpf;
+      if (naturalidade) this.naturalidade = naturalidade;
+      if (dataDeNascimento) this.dataDeNascimento = new Date(dataDeNascimento);
+      if (sexo) this.sexo = sexo;
+      if (corRaca) this.corRaca = corRaca;
+      if (usuario) this.usuario = usuario;
+      if (senha) this.senha = senha;
+    }
+  }
+  
+  module.exports = Pessoa;
+  
