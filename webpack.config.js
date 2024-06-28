0f8473f35aaa840be6035a4ad9c3a8dbf217626b
Victor Magalhães de Souza <victor.souza@logisticmobile.com.br>
2024-06-28 19:47:33 -0300

    Deploy 10 - Teste

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,64 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+import { resolve as _resolve, join } from 'path';
+import Dotenv from 'dotenv-webpack';
 
-module.exports = {
-  entry: './App/Components/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'frontend/public'),
-    filename: 'bundle.js',
-    publicPath: '/frontEnd/public/', // Caminho base para os arquivos estáticos
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-              context: 'frontend/public',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'frontend/public'),
-      watch: {
-        ignored: /node_modules/,
-      },
-    },
-    compress: true,
-    port: 9000,
-    historyApiFallback: true,
-    proxy: [
-      {
-        context: ['/api'],
-        target: 'https://contagemativabackend.azurewebsites.net',
-        changeOrigin: true,
-        pathRewrite: {'^/api': ''},
-      },
-    ],
-  },
-  plugins: [
-    new Dotenv()
-  ]
+export const entry = './App/Components/index.jsx';
+export const output = {
+  path: _resolve(__dirname, 'frontend/public'),
+  filename: 'bundle.js',
+  publicPath: '/frontEnd/public/',
 };
+export const module = {
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+            context: 'frontend/public',
+          },
+        },
+      ],
+    },
+  ],
+};
+export const resolve = {
+  extensions: ['.js', '.jsx']
+};
+export const devServer = {
+  static: {
+    directory: join(__dirname, 'frontend/public'),
+    watch: {
+      ignored: /node_modules/,
+    },
+  },
+  compress: true,
+  port: 9000,
+  historyApiFallback: true,
+  proxy: [
+    {
+      context: ['/api'],
+      target: 'https://contagemativabackend.azurewebsites.net',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
+  ],
+};
+export const plugins = [
+  new Dotenv()
+];
