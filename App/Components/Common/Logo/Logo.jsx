b9f691d7f6489bcd89b6f86adbc72229642c7910
Victor Magalhães de Souza <victor.souza@logisticmobile.com.br>
2024-06-29 17:52:35 -0300

    Deploy 21

--- App/Components/Common/Logo/Logo.jsx
+++ App/Components/Common/Logo/Logo.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Logo({ width, height }) {
     return (
-        <img src="https://contagemativa.github.io/frontEnd/public/img/logo-contagem-ativa.png" width={width} height={height}></img>
+        <img src="./frontEnd/public/img/logo-contagem-ativa-white.png" width={width} height={height}></img>
     )
 }
