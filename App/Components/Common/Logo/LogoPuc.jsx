da76d08d37caa5ccc146f86437778ca2549ecbda
Victor Magalhães de Souza <victor.souza@logisticmobile.com.br>
2024-06-29 17:56:24 -0300

    Deploy 22

--- App/Components/Common/Logo/LogoPuc.jsx
+++ App/Components/Common/Logo/LogoPuc.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function LogoPuc({ width, height }) {
     return (
-        <img src="./frontEnd/public/img/logo-contagem-ativa-white.png" width={width} height={height}></img>
+        <img src="./img/logo-contagem-ativa-white.png" width={width} height={height}></img>
     )
 }
